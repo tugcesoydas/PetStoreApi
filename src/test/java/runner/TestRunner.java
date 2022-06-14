bfdeb3593555caf42afc04fcb1bc2e72bb1dde3e
Tugce Soydas <81081023+tugcesoydas@users.noreply.github.com>
2022-06-14 17:14:36 +0300

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,6 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"stepDefinitions"},
-        plugin = {"json:target/cucumber.json","json:target/cucumber-reports/Cucumber.json"})
+        plugin = {"json:target/cucumber.json","json:target/cucumber-reports/Cucumber.json"},
+        dryRun = false,
+        publish =false,
+        tags = "@AllCases"
+)
 public class TestRunner {
 }
